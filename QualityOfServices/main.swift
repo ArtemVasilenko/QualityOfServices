36e71a34fe02bea97734ca245bad683680260f1d
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-07-18 20:42:37 +0300

    Initial Commit

---
+++ QualityOfServices/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  QualityOfServices
+//
+//  Created by Артем on 7/18/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
